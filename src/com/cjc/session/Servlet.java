1d788d5fc159dbc4c7e178c9449d5262e76d5825
Edward cheng <Ed@mail.com>
2020-03-01 21:12:13 +0800

    Update Servlet.java

--- src/com/cjc/session/Servlet.java
+++ src/com/cjc/session/Servlet.java
@@ -3,19 +3,47 @@ package com.cjc.session;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 @WebServlet(name = "Servlet", urlPatterns = "/Servlet")
 public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=utf-8");
         PrintWriter out = response.getWriter();
-        HttpSession hs = request.getSession();
-        hs.setAttribute("name","cjc");
+        System.out.println("Servlet");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.doPost(request, response);
+    }
+
+    public void destroy() {
+        super.destroy();
+        System.out.println("退出servlet");
+        String filepath = this.getServletContext().getRealPath("record.txt");
+        try {
+            FileWriter fileWriter = new FileWriter(filepath);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            String nums = (String) this.getServletContext().getAttribute("nums");
+            bufferedWriter.write(nums);
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void init() throws ServletException{
+        System.out.println("进入servlet");
+        String filepath = this.getServletContext().getRealPath("record.txt");
+        try {
+            FileReader fr = new FileReader(filepath);
+            BufferedReader bufferedReader = new BufferedReader(fr);
+            String nums = bufferedReader.readLine();
+            this.getServletContext().setAttribute("nums",nums);
+            bufferedReader.close();
+            fr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
